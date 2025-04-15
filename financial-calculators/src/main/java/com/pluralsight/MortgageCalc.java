be0a100a3ff62abdde1a607c613dd93a88b7f777
Gio <giovannizavala82@gmail.com>
2025-04-15 11:44:53 -0500

    Mortgage calculator completed

--- financial-calculators/src/main/java/com/pluralsight/MortgageCalc.java
+++ financial-calculators/src/main/java/com/pluralsight/MortgageCalc.java
@@ -1,7 +1,40 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class MortgageCalc {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the principal loan amount : ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter the annual interest rate (as a percentage, ex: 7.625): ");
+        double annualRate = scanner.nextDouble();
+
+        System.out.print("Enter the loan term in years: ");
+        int loanYears = scanner.nextInt();
+        int totalPayments = loanYears * 12;
+        double monthlyRate = annualRate / 100 / 12;
+
+        double numerator = monthlyRate * Math.pow(1 + monthlyRate, totalPayments);
+        double denominator = Math.pow(1 + monthlyRate, totalPayments) - 1;
+        double monthlyPayment = principal * (numerator / denominator);
+
+        double totalPaid = monthlyPayment * totalPayments;
+
+        double totalInterest = totalPaid - principal;
+        /*
+        % is a placeholder for a variable value
+        .2f specifies it only wants 2 digits after decimal point (floating point number)
+        $ means its money
+        %n adds a new line after the output
+        */
+        System.out.println("\n- Mortgage Details -");
+        System.out.printf("Monthly Payment: $%.2f%n", monthlyPayment);
+        System.out.printf("Total Interest Paid: $%.2f%n", totalInterest);
+
+        scanner.close();
 
     }
 }
