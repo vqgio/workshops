cd9e4e97bb2d83dcdefa73276d03c720d227307c
Gio <giovannizavala82@gmail.com>
2025-05-09 10:57:48 -0500

    started on dealership class

--- CarDealership/src/main/java/com/pluralsight/Dealership.java
+++ CarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership() {
+        this.inventory = new ArrayList<Vehicle>();
+    }
+    public ArrayList<Vehicle> getAllVehicles() {
+        return null;
+    }
+    public void removeVehicle(Vehicle vehicle){
+    }
+
 }
