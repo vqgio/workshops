949dc5957521cc665db0ba6089d57e6762626b72
Gio <giovannizavala82@gmail.com>
2025-05-09 15:24:52 -0500

    got some of user interface done.

--- CarDealership/src/main/java/com/pluralsight/UserInterface.java
+++ CarDealership/src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,48 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UserInterface {
+    private Dealership dealership;
+
+    private void init() {
+        dealership = DealershipFileManager.getDealership();
+    }
+
+    public void display() {
+        init();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while(running) {
+            displayMenu();
+            System.out.println("Enter command: ");
+            String command = scanner.nextLine();
+
+            switch(command) {
+                case "1":
+                    processAllVehicleRequest();
+                    break;
+                case "q":
+                    running = false;
+                    System.out.println("quitting program. Exiting now....");
+                    break;
+                default:
+                    System.out.println("Sorry that is not an option, try again!");
+            }
+        }
+        scanner.close();
+    }
+    private void processAllVehicleRequest() {
+
+    }
+    private void displayVehicles(ArrayList<Vehicle> vehicles) {
+
+    }
+    private void displayMenu() {
+
+    }
+
 }
