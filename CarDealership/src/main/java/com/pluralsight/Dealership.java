5cb2a4d3b27cfef1fe1fe856eb86cd755526fd9d
Gio <giovannizavala82@gmail.com>
2025-05-09 14:52:48 -0500

    got vehicle, dealership and file manager class

--- CarDealership/src/main/java/com/pluralsight/Dealership.java
+++ CarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 
 public class Dealership {
     private ArrayList<Vehicle> inventory;
+    private String location;
+    private String name;
 
-    public Dealership() {
+    public Dealership(String name, String location) {
         this.inventory = new ArrayList<Vehicle>();
+        this.location = location;
+        this.name = name;
     }
     public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+    public Vehicle searchByMake(String make) {
         return null;
     }
-    public void removeVehicle(Vehicle vehicle){
+    public Vehicle searchByModel(String model) {
+        return null;
+    }
+    public Vehicle searchByYear(int year) {
+        return null;
+    }
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+    public void removeVehicle(Vehicle vehicle) {
     }
 
 }
--- CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
+    private static final string FILE_PATH = "inventory.csv";
+
+    public static Dealership getDealership() {
+        Dealership dealership = new Dealership("Default, Location unknown");
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                if(data.length == 8) {
+                    int vehicleId = Integer.parseInt(data[0]);
+                    int modelYear = Integer.parseInt(data[1]);
+                    String make = data[2];
+                    String model = data[3];
+                    String carType = data[4];
+                    String color = data[5];
+                    int mileage = Integer.parseInt(data[6]);
+                    double price = Double.parseDouble(data[7]);
+                    Vehicle vehicle = new Vehicle(vehicleId, modelYear, make, model, carType, color, mileage, price);
+                    dealership.addVehicle(vehicle);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR, failed reading inventory, please try again" + e.getMessage());
+        }
+        return dealership;
+    }
+    public static void saveDealership(Dealership dealership) {
+        //for later
+    }
 }
